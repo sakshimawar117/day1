187788818695968c852c2885c915ffb5de735e58
sakshimawar117 <sakshimawar10@gmail.com>
2021-09-15 11:54:50 +0530

    initial project

---
+++ asset/javascript/index.js
@@ -0,0 +1,111 @@
+
+
+/*write a single line comment whichs says, comments can make code readable*/
+
+//comment can make code readable
+
+//Welcome to JavaScript 
+
+/*  comments can make
+ code readable easy to 
+ reuse and informative 
+ */
+//Declare variables and assign string,bollean ,undefined and null data types
+
+
+let string='sakshi'
+console.log(string)
+
+let number="9"
+
+console.log(number)
+
+let boolean="false"
+
+console.log(boolean)
+
+let nul=""
+
+console.log(null)
+
+let u;
+
+console.log(u)
+
+//use the javascript type of operator to check diffrent type of data types .check the data type of each variable 
+
+console.log(2+9)
+console.log(9*7)
+console.log( 3%5)
+console.log(5/8)
+
+
+let a=8
+let b=9
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+console.log(typeof 'sakshi')
+console.log(typeof 8)
+console.log (typeof false)
+console.log (typeof null)
+console.log(typeof abc)
+
+// declare four variable without assign value
+
+let w;
+let x;
+let y;
+let z;
+console.log(w,x,y,z)
+
+//declare four varialbew with assign values
+let e=4;
+let f=5;
+let g=8;
+let h=6;
+
+console.log(e+f+g+h)
+
+//declare to store your first name ,last name,marital status,country and age in multiple lines
+
+let firstname="Sakshi"
+let lastname="Mawar"
+let Maritalstatus="unmarried"
+let country="India"
+let age="20"
+console.log(firstname)
+
+console.log(lastname)
+
+console.log(Maritalstatus)
+
+console.log(country)
+
+console.log(age)
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+let studentname="Sakshi"
+let studentlastname="Mawar"
+let status="unmarried"
+let city="Indore"
+let studentage="20"
+
+console.log(studentname,studentlastname,status,city,studentage)
+
+//Declare two variable myage and yourage and assign them inital values and log to the browser console
+
+var myage="19"
+var yourage="22"
+
+console.log(myage)
+console.log(yourage)
+
+
+
+
+let clickfunc =() =>{
+    alert( "hello ")
+}
